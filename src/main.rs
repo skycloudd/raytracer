b24778bfa1e8a0157f1561dacb499c16f3bc43d2
skycloudd <51929172+skycloudd@users.noreply.github.com>
2023-09-10 18:14:36 +0200

    add interchangeable scenes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use camera::Camera;
-use colour::rgb;
 use glam::Vec3;
-use materials::{Lambertian, Metal};
-use objects::{Sphere, World};
-use std::rc::Rc;
 
 mod camera;
 mod colour;
@@ -12,40 +8,25 @@ mod interval;
 mod materials;
 mod objects;
 mod ray;
+mod scenes;
 
 const WIDTH: u32 = 800;
 const ASPECT_RATIO: f32 = 16.0 / 9.0;
 const VERTICAL_FOV: f32 = 60.0;
 
-const LOOK_FROM: Vec3 = Vec3::new(-0.7, 1.0, 0.6);
-const LOOK_AT: Vec3 = Vec3::new(0.0, 0.5, -1.0);
+const LOOK_FROM: Vec3 = Vec3::new(0.0, 0.0, 2.0);
+const LOOK_AT: Vec3 = Vec3::new(0.0, 0.0, 0.0);
 
-const SAMPLES_PER_PIXEL: u32 = 16;
-const MAX_DEPTH: u16 = 100;
+const SAMPLES_PER_PIXEL_CONFIG: u32 = 6;
+const SAMPLES_PER_PIXEL: u32 = SAMPLES_PER_PIXEL_CONFIG * SAMPLES_PER_PIXEL_CONFIG;
+const MAX_DEPTH: u16 = 50;
 
-const DEFOCUS_ANGLE: f32 = 0.5;
+const DEFOCUS_ANGLE: f32 = 0.0;
 const FOCUS_DISTANCE: f32 = 1.5;
 
 fn main() {
-    let material_ground = Rc::new(Lambertian::new(rgb(0, 211, 255))) as _;
-    let material_center = Rc::new(Lambertian::new(rgb(255, 211, 0))) as _;
-    let material_left = Rc::new(Metal::new(rgb(127, 50, 0), 0.05)) as _;
-    let material_right = Rc::new(Metal::new(rgb(50, 127, 50), 0.01)) as _;
-
-    let world = World::new(vec![
-        Rc::new(Sphere::new(
-            Vec3::new(0.0, -100.0, -1.0),
-            100.0,
-            material_ground,
-        )),
-        Rc::new(Sphere::new(
-            Vec3::new(-0.1, 0.5, -1.0),
-            0.5,
-            material_center,
-        )),
-        Rc::new(Sphere::new(Vec3::new(-1.2, 0.4, -1.0), 0.4, material_left)),
-        Rc::new(Sphere::new(Vec3::new(1.1, 0.5, -0.3), 0.5, material_right)),
-    ]);
+    let world = scenes::three_spheres();
+    // let world = scenes::rainbow_spheres();
 
     let mut camera = Camera::new();
 
---
+++ src/scenes.rs
@@ -0,0 +1,81 @@
+use crate::colour::rgb;
+use crate::materials::{Lambertian, Metal};
+use crate::objects::{Sphere, World};
+use glam::Vec3;
+use std::rc::Rc;
+
+fn ground_material() -> Lambertian {
+    Lambertian::new(rgb(0, 211, 255))
+}
+
+fn ground_sphere() -> Sphere {
+    Sphere::new(
+        Vec3::new(0.0, -100.5, -1.0),
+        100.0,
+        Rc::new(ground_material()),
+    )
+}
+
+pub fn three_spheres() -> World {
+    let material_center = Rc::new(Lambertian::new(rgb(255, 211, 0)));
+    let material_left = Rc::new(Metal::new(rgb(127, 50, 0), 0.05));
+    let material_right = Rc::new(Metal::new(rgb(50, 127, 50), 0.01));
+
+    let three_spheres = World::new(vec![
+        Rc::new(ground_sphere()),
+        Rc::new(Sphere::new(
+            Vec3::new(-0.5, 0.0, -1.0),
+            0.5,
+            material_center,
+        )),
+        Rc::new(Sphere::new(Vec3::new(-1.6, -0.1, -0.8), 0.4, material_left)),
+        Rc::new(Sphere::new(Vec3::new(0.7, 0.0, -0.5), 0.5, material_right)),
+    ]);
+
+    three_spheres
+}
+
+pub fn rainbow_spheres() -> World {
+    const RADIUS: f32 = 0.15;
+
+    let material_red = Rc::new(Metal::new(rgb(255, 0, 0), 0.1)) as _;
+    let material_orange = Rc::new(Metal::new(rgb(255, 127, 0), 0.1)) as _;
+    let material_yellow = Rc::new(Metal::new(rgb(255, 255, 0), 0.1)) as _;
+    let material_green = Rc::new(Metal::new(rgb(0, 255, 0), 0.1)) as _;
+    let material_blue = Rc::new(Metal::new(rgb(0, 0, 255), 0.1)) as _;
+    let material_indigo = Rc::new(Metal::new(rgb(75, 0, 130), 0.1)) as _;
+
+    World::new(vec![
+        Rc::new(ground_sphere()),
+        Rc::new(Sphere::new(
+            Vec3::new(-0.5, 1.0 - 0.5 + RADIUS, 0.0),
+            RADIUS,
+            material_red,
+        )),
+        Rc::new(Sphere::new(
+            Vec3::new(-0.25, 0.8 - 0.5 + RADIUS, 0.2),
+            RADIUS,
+            material_orange,
+        )),
+        Rc::new(Sphere::new(
+            Vec3::new(0.0, 0.6 - 0.5 + RADIUS, 0.4),
+            RADIUS,
+            material_yellow,
+        )),
+        Rc::new(Sphere::new(
+            Vec3::new(0.25, 0.4 - 0.5 + RADIUS, 0.6),
+            RADIUS,
+            material_green,
+        )),
+        Rc::new(Sphere::new(
+            Vec3::new(0.5, 0.2 - 0.5 + RADIUS, 0.8),
+            RADIUS,
+            material_blue,
+        )),
+        Rc::new(Sphere::new(
+            Vec3::new(0.75, 0.0 - 0.5 + RADIUS, 1.0),
+            RADIUS,
+            material_indigo,
+        )),
+    ])
+}
