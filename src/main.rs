442aa02aa33f7c6e3cf50c86fbdbf470f0cbbb5d
skycloudd <51929172+skycloudd@users.noreply.github.com>
2023-09-10 05:40:46 +0200

    change parameters

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,18 @@ mod materials;
 mod objects;
 mod ray;
 
-const WIDTH: u32 = 1920;
+const WIDTH: u32 = 800;
 const ASPECT_RATIO: f32 = 16.0 / 9.0;
 const VERTICAL_FOV: f32 = 60.0;
 
 const LOOK_FROM: Vec3 = Vec3::new(-0.7, 1.0, 0.6);
 const LOOK_AT: Vec3 = Vec3::new(0.0, 0.5, -1.0);
 
-const SAMPLES_PER_PIXEL: u32 = 100;
+const SAMPLES_PER_PIXEL: u32 = 16;
 const MAX_DEPTH: u16 = 100;
 
-const DEFOCUS_ANGLE: f32 = 2.0;
-const FOCUS_DISTANCE: f32 = 1.2;
+const DEFOCUS_ANGLE: f32 = 0.5;
+const FOCUS_DISTANCE: f32 = 1.5;
 
 fn main() {
     let material_ground = Rc::new(Lambertian::new(rgb(0, 211, 255))) as _;
